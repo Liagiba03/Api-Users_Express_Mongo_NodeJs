4ca49110689e99f4844b020dd28bd367f1e401f1
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-02-26 12:12:58 -0600

    Add index file

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const PORT = 3000;
+
+// Crear la instacia del servidor
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//Defibir la escucha
+app.listen(PORT,()=>{
+    console.log("Server running in http://localhost:"+PORT)
+});
+
+//Definir la primera ruta
+app.get('/',(req,res)=>{
+    res.json({msj: "Hola desde express usando json"})
+});
