01765eea4127bded127578ec100a8a3746a5f94b
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-03-05 23:39:37 -0600

    Add route for create user

--- Routes/usersRoutes.js
+++ Routes/usersRoutes.js
@@ -3,10 +3,7 @@ const { createUserController, getUsersController, getUserIdController, getUserNa
 const router = require('express').Router();;
 
 router.post('/create-user', createUserController);
-router.get('/show-all-users',getUsersController);
-router.get('/show-user/:id',getUserIdController);
-router.get('/show-user-name/:name',getUserNameController);
-router.post('/update-user/:id', setUpdateUserController);
-router.delete('/delete-user/:name',deleteUserController);
+
 
 module.exports = router;
+
