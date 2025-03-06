c106d713301fc22ae0a16d3ee9e0ba8b36c94a4f
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-03-05 23:41:01 -0600

    Update routes add -> show user by id

--- Routes/usersRoutes.js
+++ Routes/usersRoutes.js
@@ -4,6 +4,7 @@ const router = require('express').Router();;
 
 router.post('/create-user', createUserController);
 router.get('/show-all-users',getUsersController);
+router.get('/show-user/:id',getUserIdController);
 
 
 module.exports = router;
