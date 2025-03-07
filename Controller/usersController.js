00a8a5a4cfa9e04b4bc91325b2c11d23eec18ca5
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-03-06 23:08:16 -0600

    Update add controller -Create User

--- Controller/usersController.js
+++ Controller/usersController.js
@@ -1,5 +1,6 @@
 const { json } = require('express');
-const {createUser, getUsers, getUserId, getUserName, setUpdateUser, deleteUser} = require('../Service/usersService.js');
+const {createUser, getUsers, getUserId, getUserName, 
+    setUpdateUser, deleteUser} = require('../Service/usersService.js');
 const { use } = require('../Routes/usersRoutes.js');
 
 //CREATE USER
@@ -13,62 +14,4 @@ const createUserController= async (req,res)=>{
     }
 }
 
-//SHOW ALL USERS
-const getUsersController = async (req, res)=>{
-    const users = await getUsers();
-    if(users){
-        return res.status(200).json(users);
-    }else{
-        return res.status(500).json({message: 'DB no connected'});
-    }
-}
 
-// SHOW USER BY ID
-const getUserIdController = async (req, res)=>{
-    const {id} = req.params;
-    const userId = await getUserId(id);
-    if(userId){
-        return res.status(200).json(userId);
-    }else{
-        return res.status(500).json({message: 'DB no connected'});
-    }
-}
-
-// SHOW USER BY NAME
-const getUserNameController = async (req, res)=>{
-    const {name} = req.params;
-    const userName = await getUserName(name);
-    console.log(userName);
-    if(userName){
-        return res.status(200).json(userName);
-    }else if (userName === null){
-        return res.status(404).json({message: 'User not found'});
-    }else{
-        return res.status(500).json({message: 'DB no connected'});
-    }
-}
-
-// UPDATE USER
-const setUpdateUserController = async (req, res)=>{
-    const {id} = req.params;
-    const data = req.body;
-    const updateUser = await setUpdateUser(id, data);
-    if(updateUser){
-        return res.status(200).json(updateUser);
-    }else{
-        return res.status(500).json({message:`DB no connected`});
-    }
-}
-
-// DELETE USER
-const deleteUserController = async (req, res)=>{
-    const {name} = req.params;
-    const deletedUser = await deleteUser(name);
-    if(deletedUser){
-        return res.status(200).json(deletedUser);
-    }else{
-        return res.status(500).json({message:`DB no connected`});
-    }
-}
-
-module.exports = {createUserController, getUsersController, getUserIdController, getUserNameController, setUpdateUserController, deleteUserController};
