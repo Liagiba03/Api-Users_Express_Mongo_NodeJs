e4a1c0769b664ea2edb51420d103ca8157237434
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-04-12 21:58:04 -0600

    Update code

--- Routes/usersRoutes.js
+++ Routes/usersRoutes.js
@@ -1,6 +1,6 @@
 const { createUserController, getUsersController, getUserIdController, getUserNameController, setUpdateUserController, deleteUserController } = require('../Controller/usersController');
 
-const router = require('express').Router();;
+const router = require('express').Router();
 
 router.post('/create-user', createUserController);
 router.get('/show-all-users',getUsersController);
